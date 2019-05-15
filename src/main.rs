17e40e4800e5b5e0c5757f4180e3edf7db2a73d2
Tom Daffin <tom@greenpear.com>
2019-05-15 00:21:38 -0600

    Change to use a toggle to switch between renderers

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,97 @@
 mod ui;
 
 mod gl_rend;
-mod gl;
-
 mod pist_rend;
-mod pist;
 
-use piston_window::AdvancedWindow;
+use piston_window::*;
+use gfx_graphics::Gfx2d;
+
+use opengl_graphics::{GlGraphics, OpenGL};
 
 fn main() {
-    let width = 320;
+    let window_width = 640;
 
-    let mut gl = gl::Win::new(width);
-    while let Some(e) = gl.next_event(){
-        gl.do_event(e);
-    }
+    // window setup
+    let opengl = OpenGL::V3_2;
+    let settings = WindowSettings::new("opengl_graphics::GlGraphics vs piston_window::G2d",
+        [window_width, 480])
+        .opengl(opengl)
+        .srgb(false)
+        .samples(4);
+    let mut window: PistonWindow = settings.build()
+        .expect("Could not create window");
 
-    let mut pist = pist::Win::new(width);
-    if let Some(glpos) = gl.window.get_position() {
-        let mut pos = glpos;
-        pos.x += width as i32 + 10;
-        pist.window.set_position(pos);
-    }
-    while let Some(e) = pist.next_event(){
-        pist.do_event(e);
+    // conrod setup
+    let size = window.size();
+    let mut ui = conrod_core::UiBuilder::new([size.width, size.height]).build();
+    let font = conrod_core::text::Font::from_bytes(include_bytes!("../assets/ProggyClean.ttf") as &[u8])
+        .expect("failed to load font");
+
+    ui.theme.font_id = Some(ui.fonts.insert(font));
+    ui.theme.shape_color = conrod_core::color::CHARCOAL;
+    ui.theme.label_color = conrod_core::color::WHITE;
+
+    let gui = ui::Gui::new(&mut ui);
+    let gui_width = size.width/2.0 - 60.0;
+
+    // event loop setup
+    let mut events = Events::new(EventSettings::new().swap_buffers(false));
+
+    // Setup similar to all_piston_window from conrod examples
+    let mut g2d = Gfx2d::new(opengl, &mut window.factory);
+    let mut pist_render = pist_rend::GuiRender::new(1024, 1024, &mut window);
+
+    // Setup similar to example given in image in piston2d-graphics
+    // https://github.com/PistonDevelopers/graphics/blob/master/src/image.rs
+    let mut gl_graphics = GlGraphics::new(opengl);
+    let mut gl_render = gl_rend::GuiRender::new(1024, 1024);
+
+    let mut state = ui::GuiState { toggle: true };
+
+    // Main event loop
+    while let Some(e) = events.next(&mut window){
+        let window = &mut window;
+
+        let g2d = &mut g2d;
+        let pist_render = &mut pist_render;
+
+        let gl_graphics = &mut gl_graphics;
+        let gl_render = &mut gl_render;
+
+        let size = window.size();
+        if let Some(e) = conrod_piston::event::convert(e.clone(), size.width, size.height) {
+            ui.handle_event(e);
+        }
+        if let Some(_) = e.update_args() {
+            gui.update(&mut ui, &mut state, 0.0, gui_width);
+        }
+
+        if let Some(r) = e.render_args() {
+            if let Some(primitives) = ui.draw_if_changed() {
+                if state.toggle {
+                    gl_graphics.draw(r.viewport(), |c, g| {
+                        graphics::clear([0.0, 0.0, 0.0, 1.0], g);
+                        gl_render.draw_primitives(primitives, c, g);
+                    });
+                } else {
+                    //g2d.draw(r.viewport(), |c, g| {
+                    window.window.make_current();
+                    //window.g2d.draw(
+                    g2d.draw(
+                        &mut window.encoder,
+                        &window.output_color,
+                        &window.output_stencil,
+                        r.viewport(),|c, g|{
+                    //window.draw_2d(&e, |c, g|{
+                        graphics::clear([0.0, 0.0, 0.0, 1.0], g);
+                        pist_render.draw_primitives(primitives, c, g);
+                    });
+                    window.encoder.flush(&mut window.device);
+                }
+                window.window.swap_buffers();
+            }
+        }
+
     }
     
 }
--- src/ui.rs
+++ src/ui.rs
@@ -6,23 +6,28 @@ use conrod_core::{
     Borderable,
     Ui,
     widget_ids,
-    widget::Widget,
+    widget::{Widget, Text, Toggle, List},
 };
 
 widget_ids! {
     struct Ids {
+        title,
+        text,
+        toggle,
         list,
     }
 }
 
+pub struct GuiState {
+    pub toggle: bool,
+}
+
 pub struct Gui {
-    ui: Ui,
     ids: Ids,
 }
 
 impl Gui {
-    pub fn new(width: f64, height: f64) -> Self {
-        let mut ui = conrod_core::UiBuilder::new([width, height]).build();
+    pub fn new(ui: &mut Ui) -> Self {
         let ids = Ids::new(ui.widget_id_generator());
 
         let font = conrod_core::text::Font::from_bytes(include_bytes!("../assets/ProggyClean.ttf") as &[u8])
@@ -33,43 +38,58 @@ impl Gui {
         ui.theme.label_color = conrod_core::color::WHITE;
 
         Self {
-            ui,
             ids,
         }
     }
 
-    pub fn handle_event(&mut self, e: conrod_core::event::Input) {
-        self.ui.handle_event(e);
-    }
+    pub fn update(&self, ui: &mut Ui, state: &mut GuiState, x_off: f64, width: f64) {
+        let ui = &mut ui.set_widgets();
 
-    pub fn update(&mut self) {
-        let ui = &mut self.ui.set_widgets();
-        let ids = &self.ids;
+        Text::new("Graphics Scizzor Inconsistency")
+            .mid_top_with_margin_on(ui.window, 24.0)
+            .set(self.ids.title, ui);
 
-        let (mut list_items_iter, scrollbar) = conrod_core::widget::List::flow_down(20)
-            .top_left_with_margins_on(ui.window, ui.win_h/2.0-50.0, 30.0)
+        Text::new("Click toggle button below to switch between renderers.\nOnly one of the two will give correct results")
+            .down_from(self.ids.title, 18.0)
+            .align_middle_x_of(self.ids.title)
+            .padded_w_of(ui.window, 10.0)
+            .wrap_by_word()
+            .set(self.ids.text, ui);
+
+        let label = if state.toggle {
+            "opengl_graphics"
+        } else {
+            "gfx_graphics"
+        };
+        for v in Toggle::new(state.toggle)
+            .down_from(self.ids.text, 10.0)
+            .align_middle_x_of(self.ids.title)
+            .w_h(240.0, 36.0)
+            .label(label)
+            .set(self.ids.toggle, ui)
+        {
+            state.toggle = v;
+        }
+
+        let (mut list_items_iter, scrollbar) = List::flow_down(20)
+            .top_left_with_margins_on(ui.window, ui.win_h/2.0-50.0, 30.0 + x_off)
             .item_size(35.0)
             .h(ui.win_h/2.0-30.0)
-            .w(ui.win_w-60.0)
+            .w(width)
             .scrollbar_on_top()
-            .set(ids.list, ui);
+            .set(self.ids.list, ui);
 
         scrollbar.map(|s| s.set(ui));
 
-        let mut i = 1;
         while let Some(item) = list_items_iter.next(ui) {
-            let lbl = format!("Item {}", i);
+            let lbl = format!("Item {}", item.i);
             let button = conrod_core::widget::Button::new()
                 .label(&lbl)
                 .border(1.0)
                 .border_color(conrod_core::color::WHITE)
                 .label_font_size(15);
             item.set(button, ui);
-            i += 1;
         }
     }
 
-    pub fn draw_if_changed(&self) -> Option<conrod_core::render::Primitives> {
-        self.ui.draw_if_changed()
-    }
 }
